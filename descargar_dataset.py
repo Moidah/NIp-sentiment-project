527eef7824d7bde66dcbafeb5cb73bb1b90fc792
Moidah <moisesdavidarh@gmail.com>
2026-09-01 10:48:09 -0500

    descarga de dataset lista

---
+++ descargar_dataset.py
@@ -0,0 +1,7 @@
+"""Descarga el dataset real de reseñas de IMDB (50,000 reseñas, ~64MB)."""
+import urllib.request
+
+URL = "https://raw.githubusercontent.com/SK7here/Movie-Review-Sentiment-Analysis/master/IMDB-Dataset.csv"
+print("Descargando dataset de IMDB (~64MB, puede tardar un minuto)...")
+urllib.request.urlretrieve(URL, "imdb_reviews.csv")
+print("Descarga completa: imdb_reviews.csv")
